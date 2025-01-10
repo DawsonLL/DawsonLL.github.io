4546193ac8e30ca67a4d854eece95b02ea6f94ad
Long Lam <long.lam.1254@gmail.com>
2025-01-10 00:58:39 -0800

    Added dark mode functionality.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 /* Use to manipulate CSS rules/declarations as well as data on screen for the user */
-
+function darkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  } 
 
