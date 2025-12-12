22fb564489cef842f180da2f06573d370634437f
Long Lam <long.lam.1254@gmail.com>
2025-12-12 00:45:14 -0800

    Added localStorage to retain page state, properly aligned footer to always be botto m of page, reserved transitions to only activate when toggling page theme

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-/* Use to manipulate CSS rules/declarations as well as data on screen for the user */
-function darkMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-  }
+document.documentElement.classList.add("no-transition");
 
-  
+document.addEventListener("DOMContentLoaded", () => {
+    const savedMode = localStorage.getItem("theme");
+
+    if (savedMode === "dark") {
+        document.body.classList.add("dark-mode");
+    }
+
+    setTimeout(() => {
+        document.documentElement.classList.remove("no-transition");
+    }, 5);
+});
+
+function darkMode() {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+    }
+    else {
+        localStorage.setItem("theme", "light");
+    }
+}
